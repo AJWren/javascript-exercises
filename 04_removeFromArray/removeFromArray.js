b952582abd92775c1fac01661dd3e4732e039a0f
Adam <adam.wren@ten10.com>
2024-06-21 15:52:50 +0100

    completed 04, working on 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arr, ...args) {
+    let newArray = [];
+
+    arr.forEach((item) => {
+    
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = 0
+    for (let i = a; i <= b; i++){
+      total += i
+      return total
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
