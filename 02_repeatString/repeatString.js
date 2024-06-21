70653ecd33f1a03d5165c44942d20803f731179d
Adam <adam.wren@ten10.com>
2024-06-21 14:28:39 +0100

    Completed exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
+const a = 'Hey'
+const times = 3
+
+const repeatString = function(word, a) {
+    if (a < 0){
+        return "ERROR"
+    }
+    else return word.repeat(a)
 };
+
+repeatString(a, times)
 
 // Do not edit below this line
 module.exports = repeatString;
