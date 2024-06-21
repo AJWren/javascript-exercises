e3fc96f66e0babe38376ab5711103297aa4b5525
Adam <adam.wren@ten10.com>
2024-06-21 14:35:59 +0100

    Completed execrise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("")
+    let reverseString = splitString.reverse()
+    let completedString = reverseString.join("")
+    return completedString
 };
 
 // Do not edit below this line
