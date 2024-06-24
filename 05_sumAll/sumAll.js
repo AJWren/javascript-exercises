c2066138c3ae3e9b0c05a540e9b7add1dead6fb8
Adam <adam.wren@ten10.com>
2024-06-24 10:27:55 +0100

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,31 @@
 const sumAll = function(a, b) {
+  if (a < 0 || b < 0){
+    return "ERROR"
+  }
+  else if (!Number.isInteger(a) || !Number.isInteger(b)){
+    return "ERROR"
+  }
+  else if (a > b){
+    let totalA = 0
+    let resultsA = []
+    for (let i = b; i <= a; i++){
+      totalA += i
+      resultsA.push(totalA)
+    }
+    return resultsA.slice(-1).pop()
+  }
+  else {
     let total = 0
+    let results = []
     for (let i = a; i <= b; i++){
       total += i
-      return total
+      results.push(total)
+  
     }
-};
+    return results.slice(-1).pop()
+  }
+
+};  
 
 
 // Do not edit below this line
