7a77944991bbf607baec768d6719cfd13b98401e
Adam <adam.wren@ten10.com>
2024-07-02 11:39:31 +0100

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((a, b) => a + b, 0)
+};
+
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b)
+};
+
+const power = function(a, b) {
+  return Math.pow(a, b)
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0){
+    return 1;
+  }
+  else{
+    for(let i = a -1; i >= 1; --i){
+     a *= i;
+    }
+	return a
+  }
 };
 
 // Do not edit below this line
