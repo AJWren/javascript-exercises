7d5439dd899821faccc5a97b72226b802dcc3705
Adam <adam.wren@ten10.com>
2024-07-02 14:27:41 +0100

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let fib = [0, 1];
+    if(a < 0){
+        return "OOPS"
+    }
+    else{
+        for (let i = 2; i <= 100; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+        }
+        return fib.at(a)
+    }
 
 };
 
