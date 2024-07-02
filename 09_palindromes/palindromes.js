67c9e638271ae4ffde5fecde249918a0cdeb8b61
Adam <adam.wren@ten10.com>
2024-07-02 12:12:05 +0100

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const lowerString = string.toLowerCase()
+    const plainString = lowerString.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replace(/\s/g, '');
+
+    const reversedString = plainString.split("").reverse().join("")
+
+
+    return plainString === reversedString
 };
 
 // Do not edit below this line
